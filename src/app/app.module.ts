a0044d9cb6f4ecfd02d4db8482b49ea7d89ecd51
Ahmed Chowdhury <rupock@yahoo.com>
2017-09-22 11:20:09 -0400

    configure welcome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path:'home',component:WelcomeComponent
+      },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {path:'**',component:PagenotfoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
